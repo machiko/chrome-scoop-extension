1f82746e69f943124d9665388ed6779767c202a1
reyes <machikoreo@gmail.com>
2015-08-12 13:40:37 +0800

    將標題進行 ckip 分詞動作

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,4 +1,7 @@
 // Content script
+var gapi_key = "AIzaSyBBnkRPORCp9RujLhF4kEjkYCXOsa72mgI";
+var gcse_cx = "001325834152955743717:jmzdtntzo60";
+
 /* Listen for messages */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     /* If the received message has the expected format... */
@@ -18,7 +21,31 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
         	"org": org,
         	"content": content
         }
-        console.log(news_json);
+
+        // 針對 title 進行 ckip 處理
+        $.post('https://localhost:8443/Scoop/api/ckip',
+        {
+          content: news_json.title,
+        }, function(result)
+        {
+          console.log(result);
+        }, 'json');
+
+        // 使用 google custom search
+        $.get('https://www.googleapis.com/customsearch/v1',
+        {
+          key: gapi_key,
+          cx: gcse_cx,
+          q: "課綱爭議"
+        }, function(result) {
+          var result_len = result.items.length
+          for (var i = 0; i < result_len; i++)
+          {
+
+          }
+          console.log(result.items);
+        }, 'json');
+        // console.log(news_json);
         // sendResponse($(document).find('html').children("body").find("#mediaarticlebody").text());
         sendResponse(JSON.stringify(news_json));
   //       var str = "aa<b>1;2'3</b>hh<b>aaa</b>..\n.<b>bbb</b>\nblabla..";
