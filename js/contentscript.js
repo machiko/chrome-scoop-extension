a389aeb477b9cb3bf1c1ef7c3b61272a7871b4bd
reyes <machikoreo@gmail.com>
2015-09-15 14:17:41 +0800

    - 更新 icon - 更新提示套件 -> notify.js

--- js/contentscript.js
+++ js/contentscript.js
@@ -8,52 +8,81 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text && (msg.text == "report_back")) {
         /* Call the specified callback, passing
            the web-pages DOM content as argument */
-
+        // $.notify("擷取網頁內容中...", "success");
         var title = $(document).find('html').children("body").find("#mediaarticlehead .headline").text();
         var author = $(document).find('html').children("body").find("#mediaarticlehead .fn").text();
         var org = $(document).find('html').children("body").find("#mediaarticlehead .provider.org").text();
-        var content = $(document).find('html').children("body").find("#mediaarticlebody").text();
+        var content = $(document).find('html').children("body").find("#mediaarticlebody3").text();
         // sendResponse(document.all[0].outerHTML);
         // sendResponse($(document).find('html').children("body").find("#mediaarticlebody").text());
         var news_json = {
-        	"title": title,
-        	"author": author,
-        	"org": org,
-        	"content": content
+            "title": title,
+            "author": author,
+            "org": org,
+            "content": content,
+            "link": window.location.href
         }
 
-        // 針對 title 進行 ckip 處理
-        $.post('https://localhost:8443/Scoop/api/ckip',
+        $.notify("進行 ckip 標題分詞",
         {
-          content: news_json.title,
-        }, function(result)
-        {
-          console.log(result);
-        }, 'json');
-
-        // 使用 google custom search
-        $.get('https://www.googleapis.com/customsearch/v1',
-        {
-          key: gapi_key,
-          cx: gcse_cx,
-          q: "課綱爭議"
+          globalPosition: 'bottom right',
+          className: "success"
+        });
+        // step1. 針對 title 進行 ckip 處理
+        $.post('https://localhost:8443/Scoop/api/ckip', {
+            content: news_json.title,
         }, function(result) {
-          var result_len = result.items.length
-          for (var i = 0; i < result_len; i++)
-          {
+            for (var i = result.word.length - 1; i >= 0; i--) {
+                if (result.speech[i] == "PAUSECATEGORY") {
+                    result.word.splice(i, 1);
+                }
+            }
+            var ckip_search = result.word.join(" ");
+            $.notify("標題分詞結果 : " + ckip_search,
+            {
+              clickToHide: true,
+              autoHide: false,
+              globalPosition: 'bottom right',
+              className: "info"
+            });
+            console.log(ckip_search);
 
-          }
-          console.log(result.items);
+            // step2. 使用 google custom search
+            $.get('https://www.googleapis.com/customsearch/v1', {
+                key: gapi_key,
+                cx: gcse_cx,
+                q: ckip_search
+            }, function(result) {
+                $.notify("使用 google custom search 搜尋",
+                {
+                  globalPosition: 'bottom right',
+                  className: "success"
+                });
+                var result_len = result.items.length
+                for (var i = 0; i < result_len; i++) {
+                    var match_str = news_json.link;
+                    // console.log(news_json.link);
+                    // console.log(result.items[i].link);
+                    // 去除重複的網址
+                    console.log(result.items[i].link);
+                    if (match_str.match(result.items[i].link)) {
+                        result.items.splice(i, 1);
+                    }
+                }
+                // console.log(result.items);
+            }, 'json');
+
         }, 'json');
+
         // console.log(news_json);
         // sendResponse($(document).find('html').children("body").find("#mediaarticlebody").text());
         sendResponse(JSON.stringify(news_json));
-  //       var str = "aa<b>1;2'3</b>hh<b>aaa</b>..\n.<b>bbb</b>\nblabla..";
+        //       var str = "aa<b>1;2'3</b>hh<b>aaa</b>..\n.<b>bbb</b>\nblabla..";
 
-		// var match = "";
-		// var result = "";
-		// var regex = /<b>(.*?)<\/b>/ig;
-		// while (match = regex.exec(str)) { result += match[1]; }
+        // var match = "";
+        // var result = "";
+        // var regex = /<b>(.*?)<\/b>/ig;
+        // while (match = regex.exec(str)) { result += match[1]; }
 
     }
 });
